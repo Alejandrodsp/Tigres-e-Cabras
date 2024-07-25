8f960812fd355de0f9a03c094ba35d6b97d7f877
Alejandrodsp <pereira_randro@hotmail.com>
2024-07-25 20:53:43 -0300

    Corrigi movimentos e capturas validas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,9 @@ export class AppComponent implements OnInit {
   cabrasMortas = 0;
   vencedor: (null | 'goat' | 'tiger') = null;
   backgroundMusic = new Audio();
+  movimentos_validos: { [key: string]: string[] } = {"0_0":["0_1","1_0","1_1"],"0_1":["0_2","0_0","1_1"],"0_2":["0_3","0_1","1_2","1_1","1_3"],"0_3":["0_4","0_2","1_3"],"0_4":["0_3","1_4","1_3"],"1_0":["1_1","2_0","0_0"],"1_1":["1_2","1_0","2_1","0_1","2_0","0_2","2_2","0_0"],"1_2":["1_3","1_1","2_2","0_2"],"1_3":["1_4","1_2","2_3","0_3","2_2","0_4","2_4","0_2"],"1_4":["1_3","2_4","0_4"],"2_0":["2_1","3_0","1_0","1_1","3_1"],"2_1":["2_2","2_0","3_1","1_1"],"2_2":["2_3","2_1","3_2","1_2","3_1","1_3","3_3","1_1"],"2_3":["2_4","2_2","3_3","1_3"],"2_4":["2_3","3_4","1_4","3_3","1_3"],"3_0":["3_1","4_0","2_0"],"3_1":["3_2","3_0","4_1","2_1","4_0","2_2","4_2","2_0"],"3_2":["3_3","3_1","4_2","2_2"],"3_3":["3_4","3_2","4_3","2_3","4_2","2_4","4_4","2_2"],"3_4":["3_3","4_4","2_4"],"4_0":["4_1","3_0","3_1"],"4_1":["4_2","4_0","3_1"],"4_2":["4_3","4_1","3_2","3_3","3_1"],"4_3":["4_4","4_2","3_3"],"4_4":["4_3","3_4","3_3"]};
+  
+  capturas_validas: { [key: string]: string[] } = {"0_0":["0_2","2_0","2_2"],"0_1":["0_3","2_1"],"0_2":["0_4","0_0","2_2","2_0","2_4"],"0_3":["0_1","2_3"],"0_4":["0_2","2_4","2_2"],"1_0":["1_2","3_0"],"1_1":["1_3","3_1","3_3"],"1_2":["1_4","1_0","3_2"],"1_3":["1_1","3_3","3_1"],"1_4":["1_2","3_4"],"2_0":["2_2","4_0","0_0","0_2","4_2"],"2_1":["2_3","4_1","0_1"],"2_2":["2_4","2_0","4_2","0_2","4_0","0_4","4_4","0_0"],"2_3":["2_1","4_3","0_3"],"2_4":["2_2","4_4","0_4","4_2","0_2"],"3_0":["3_2","1_0"],"3_1":["3_3","1_1","1_3"],"3_2":["3_4","3_0","1_2"],"3_3":["3_1","1_3","1_1"],"3_4":["3_2","1_4"],"4_0":["4_2","2_0","2_2"],"4_1":["4_3","2_1"],"4_2":["4_4","4_0","2_2","2_4","2_0"],"4_3":["4_1","2_3"],"4_4":["4_2","2_4","2_2"]};
   
   constructor(public dialog: MatDialog) {}
 
@@ -65,40 +68,25 @@ export class AppComponent implements OnInit {
 
   realizarJogada(rowIndex: number, colIndex: number) {
     let moveu = false;
-    if (this.jogadorAtual === 'goat') {
-      if (this.cabrasTabuleiro < 20) {
-        if (this.tabuleiro[rowIndex][colIndex] === null) {
-          this.tabuleiro[rowIndex][colIndex] = this.jogadorAtual;
-          this.cabrasTabuleiro++;
-          moveu = true;
-        }
-      } else {
-        if (this.tabuleiro[rowIndex][colIndex] === this.jogadorAtual) {
-          this.pecaSelecionada = [rowIndex, colIndex];
-        } else if (this.tabuleiro[rowIndex][colIndex] === null) {
-          if (this.pecaSelecionada) {
-            const distancia = this.distanciaEuclidiana(rowIndex, colIndex, this.pecaSelecionada[0], this.pecaSelecionada[1]);
-            if (distancia === 1) {
-              this.tabuleiro[this.pecaSelecionada[0]][this.pecaSelecionada[1]] = null;
-              this.pecaSelecionada = null;
-              this.tabuleiro[rowIndex][colIndex] = this.jogadorAtual;
-              moveu = true;
-            }
-          }
-        }
+    if (this.jogadorAtual === 'goat' && this.cabrasTabuleiro < 20) {
+      if (this.tabuleiro[rowIndex][colIndex] === null) {
+        this.tabuleiro[rowIndex][colIndex] = this.jogadorAtual;
+        this.cabrasTabuleiro++;
+        moveu = true;
       }
     } else {
       if (this.tabuleiro[rowIndex][colIndex] === this.jogadorAtual) {
         this.pecaSelecionada = [rowIndex, colIndex];
       } else if (this.tabuleiro[rowIndex][colIndex] === null) {
         if (this.pecaSelecionada) {
-          const distancia = this.distanciaEuclidiana(rowIndex, colIndex, this.pecaSelecionada[0], this.pecaSelecionada[1]);
-          if (distancia === 1) {
+          const capturas_validas_posicao = this.capturas_validas[`${this.pecaSelecionada[0]}_${this.pecaSelecionada[1]}`];
+          const movimentos_validos_posicao = this.movimentos_validos[`${this.pecaSelecionada[0]}_${this.pecaSelecionada[1]}`];
+          if (movimentos_validos_posicao.find(movimento => movimento == `${rowIndex}_${colIndex}`)) {
             this.tabuleiro[this.pecaSelecionada[0]][this.pecaSelecionada[1]] = null;
             this.pecaSelecionada = null;
             this.tabuleiro[rowIndex][colIndex] = this.jogadorAtual;
             moveu = true;
-          } else if (distancia === 2) {
+          } else if (capturas_validas_posicao.find(captura => captura == `${rowIndex}_${colIndex}`)) {
             const meioX = (rowIndex + this.pecaSelecionada[0]) / 2;
             const meioY = (colIndex + this.pecaSelecionada[1]) / 2;
             if (this.tabuleiro[meioX][meioY] === 'goat') {
@@ -132,42 +120,39 @@ export class AppComponent implements OnInit {
   }
 
   todosTigresImobilizados(): boolean {
-    const direcoes = [
-      [-1, -1], [-1, 0], [-1, 1], 
-      [0, -1], [0, 1],
-      [1, -1], [1, 0], [1, 1]
-    ];
-  
-    for (let row = 0; row < this.tabuleiro.length; row++) {
-      for (let col = 0; col < this.tabuleiro[row].length; col++) {
-        if (this.tabuleiro[row][col] === 'tiger') {
-          for (const [dRow, dCol] of direcoes) {
-            const newRow = row + dRow;
-            const newCol = col + dCol;
-  
-            if (newRow >= 0 && newRow < this.tabuleiro.length && newCol >= 0 && newCol < this.tabuleiro[row].length) {
-              if (this.tabuleiro[newRow][newCol] === null) {
-                return false;
-              } else if (this.tabuleiro[newRow][newCol] === 'goat') {
-                const jumpRow = newRow + dRow;
-                const jumpCol = newCol + dCol;
-                if (jumpRow >= 0 && jumpRow < this.tabuleiro.length && jumpCol >= 0 && jumpCol < this.tabuleiro[row].length) {
-                  if (this.tabuleiro[jumpRow][jumpCol] === null) {
-                    return false;
-                  }
-                }
-              }
-            }
-          }
+    const tigres = [];
+    
+    for (let i = 0; i < this.tabuleiro.length; i++) {
+      for (let j = 0; j < this.tabuleiro[i].length; j++) {
+        if (this.tabuleiro[i][j] === 'tiger') {
+          tigres.push([i, j]);
         }
       }
     }
-    return true;
-  }
   
-
-  distanciaEuclidiana(x1: number, y1: number, x2: number, y2: number) {
-    return Math.floor(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+    for (const [x, y] of tigres) {
+      const posicao = `${x}_${y}`;
+      const movimentos = this.movimentos_validos[posicao] || [];
+      const capturas = this.capturas_validas[posicao] || [];
+  
+      for (const movimento of movimentos) {
+        const [novoX, novoY] = movimento.split('_').map(Number);
+        if (this.tabuleiro[novoX][novoY] === null) {
+          return false;
+        }
+      }
+  
+      for (const captura of capturas) {
+        const [novoX, novoY] = captura.split('_').map(Number);
+        const meioX = (x + novoX) / 2;
+        const meioY = (y + novoY) / 2;
+        if (this.tabuleiro[novoX][novoY] === null && this.tabuleiro[meioX][meioY] === 'goat') {
+          return false;
+        }
+      }
+    }
+  
+    return true;
   }
 
   mutarMusica() {
